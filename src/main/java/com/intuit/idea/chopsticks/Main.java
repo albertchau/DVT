78ca2f8e42a3c8b49dc16e1ddb15a826621eb406
albertchau <albertjmchau@gmail.com>
2015-10-27 16:29:18 -0700

    Updated Pom File

--- src/main/java/com/intuit/idea/chopsticks/Main.java
+++ src/main/java/com/intuit/idea/chopsticks/Main.java
@@ -1,11 +1,15 @@
 package com.intuit.idea.chopsticks;
 
-import org.jooq.lambda.tuple.Tuple2;
+import com.intuit.idea.chopsticks.controllers.DataParityController;
+import com.intuit.idea.chopsticks.providers.StructuredJdbcDataProvider;
+import com.intuit.idea.chopsticks.providers.VendorType;
+import com.intuit.idea.chopsticks.query.QueryService;
+import com.intuit.idea.chopsticks.query.QueryServiceBuilder;
+import com.intuit.idea.chopsticks.query.TestType;
+import com.intuit.idea.chopsticks.services.ComparisonService;
+import com.intuit.idea.chopsticks.utils.exceptions.ComparisonException;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.function.BiFunction;
-import java.util.stream.IntStream;
+import java.util.List;
 
 /**
  * Copyright 2015
@@ -15,23 +19,39 @@ import java.util.stream.IntStream;
  */
 public class Main {
 
-    static Map<Tuple2<Class<?>, Class<?>>, BiFunction> comp;
+    public static void main(String[] args) throws ComparisonException {
 
-    static {
-        comp = new HashMap<>();
-        comp.put(new Tuple2<>(String.class, Integer.class), (a, b) -> a.equals(((Integer) (((Integer) b) + 4)).toString()));
-    }
-
-    public static void main(String[] args) {
-
-        IntStream.range(1, 10)
-                .boxed()
-                .map(i -> i.toString())
-                .forEach(System.out::println);
-
-        for (int i = 0; i < 10; i++) {
-            System.out.println(i);
-        }
-
+        QueryService sourceQueryService = new QueryServiceBuilder().build("students_a", VendorType.MYSQL, TestType.FULL);
+        StructuredJdbcDataProvider source = new StructuredJdbcDataProvider(
+                VendorType.MYSQL,
+                "host",
+                "port",
+                "jdbc:mysql://localhost:3306/test",
+                "root",
+                "admin",
+                "test",
+                "hivePrincipal",
+                "students_a",
+                null,
+                sourceQueryService
+        );
+        QueryService targetQueryService = new QueryServiceBuilder().build("students_b", VendorType.MYSQL, TestType.FULL);
+        StructuredJdbcDataProvider target = new StructuredJdbcDataProvider(
+                VendorType.MYSQL,
+                "host",
+                "port",
+                "jdbc:mysql://localhost:3306/test",
+                "root",
+                "admin",
+                "test",
+                "hivePrincipal",
+                "students_b",
+                null,
+                targetQueryService
+        );
+        List<ComparisonService> allComparisons = ComparisonService.createAllComparisons(null);
+        DataParityController dataParityController =
+                new DataParityController(source, target, allComparisons);
+        dataParityController.run();
     }
 }
